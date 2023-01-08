9b211575667dca3c61144fb8ddb5f03f1b8ec5f0
osman <osmangani360@gmail.com>
2023-01-08 12:47:04 -0500

    Loop

---
+++ java/src/JavaPractice/Loop.java
@@ -0,0 +1,20 @@
+package JavaPractice;
+
+public class Loop {
+    public static void main(String[] args) {
+
+//        int value = 0;
+//
+//        while(value<10){
+//            System.out.println("Hello---------" +value);
+//            value = value +1;
+//           // System.out.println("Hello---------" +value);
+
+//            for (int i= 0; i<5 ; i= i+1 ){
+//                System.out.println("Hi there");
+
+           for (int i=0;i<5; i++){
+               System.out.printf("The value of i is: %d\n",i);
+        }
+    }
+}
--- java/src/JavaPractice/TestJava.java
+++ java/src/JavaPractice/TestJava.java
@@ -1,9 +1,10 @@
+package JavaPractice;
+
 public class TestJava {
 
     public static void main(String[] args) {
         System.out.println("How is it going");
         System.out.println("How is it going ha");
-
+        System.out.println("Hi ");
     }
-
 }
