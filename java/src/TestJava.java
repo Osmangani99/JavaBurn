c0bb58fbf375ca0fa559d1ebef7263bf331f6d7f
osman <osmangani360@gmail.com>
2023-01-08 00:54:39 -0500

    just try

--- java/src/TestJava.java
+++ java/src/TestJava.java
@@ -2,6 +2,8 @@ public class TestJava {
 
     public static void main(String[] args) {
         System.out.println("How is it going");
+        System.out.println("How is it going ha");
+
     }
 
 }
